da82f3e544de1a9fe14ddd7f16919e0733400c03
chairco <chairco@gmail.com>
2016-05-14 23:24:10 +0800

    add migrations

--- fda/settings.py
+++ fda/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'fda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -85,6 +85,13 @@ DATABASES = {
         'PASSWORD': 'nutrika',
         'HOST': 'localhost',
         'PORT': '',
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
